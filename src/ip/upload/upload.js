d419e4388196e9f10778c681b2d8c0c9a983b520
nu7124 <nadim.uddin1995@gmail.com>
2018-08-26 17:07:43 -0400

    trying to do tranactions

--- src/ip/upload/upload.js
+++ src/ip/upload/upload.js
@@ -10,6 +10,7 @@ const linnia = new Linnia(web3, ipfs, { hubAddress: '0x177bf15e7e703f4980b7ef75a
 
 
 
+
 class Upload extends Component {
 
     constructor(){
@@ -18,18 +19,18 @@ class Upload extends Component {
             ipfsHash: '',
             web3: null,
             buffer: null,
-            account: null
+            account: null,
+            file:null
         }
 
         this.captureFile = this.captureFile.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+        this.onShare = this.onShare.bind(this)
     }
 
     async componentWillMount() {
         // Get network provider and web3 instance.
         // See utils/getWeb3 for more info.
-        const [owner] = await web3.eth.getAccounts();
-        console.log("OWNER", owner)
 
         getWeb3
         .then(results => {
@@ -50,7 +51,10 @@ class Upload extends Component {
         event.preventDefault()
         const file = event.target.files[0]
         const reader = new window.FileReader()
+
         reader.readAsArrayBuffer(file)
+        this.setState({file})
+        console.log(this.state.file)
         reader.onloadend = () => {
           this.setState({ buffer: Buffer(reader.result) })
           console.log('buffer', this.state.buffer)
@@ -62,7 +66,11 @@ class Upload extends Component {
         event.preventDefault()
         console.log("ABOUT TO DEFINE records")
         const {records} = await linnia.getContractInstances();
-        const owner = "0x54CE79c444897dCC0E875409eF2b8CFd26d40Baa"
+        // const owner = "0x54CE79c444897dCC0E875409eF2b8CFd26d40Baa"
+        
+
+        const [owner] = await web3.eth.getAccounts();
+
         console.log("STATE BEFORE SUBMIT", this.state)
         console.log("ADDING...")
         ipfs.files.add(this.state.buffer, async (error, result) => {
@@ -72,21 +80,39 @@ class Upload extends Component {
             }
             this.setState({ ipfsHash: result[0].hash })
             console.log('ifpsHash', this.state.ipfsHash)
-            const hash = linnia.web3.utils.sha3(JSON.stringify(this.state.ipfsHash));
-            console.log("ADDING WITH LINNI")
-            // await records.addRecord(hash, {"test":"ing"}, this.state.ipfsHash, {
-            //     from: owner,
-            //     gas: 500000,
-            //     gasPrice: 20000000000
-            // })
+            const hash = linnia.web3.utils.sha3(JSON.stringify(this.state.buffer));
+            console.log("ADDING WITH LINNI", hash)
+            console.log("OWNER", owner)
+            await records.addRecord(hash, {"name":"test"}, this.state.ipfsHash, {
+                from: owner,
+                gas: 500000,
+                gasPrice: 20000000000
+            },)
         })
+    }
+
+    onShare(){
+
     }
 
     render(){
         return(
             <div id="upload">
+                <div>
                 <form onSubmit={this.onSubmit} >
                     <input type='file' onChange={this.captureFile} />
+                    <input type='submit' />
+                </form>
+                </div>
+                <h3>Hash: {this.state.ipfsHash}</h3>
+
+                <form onSubmit={this.onShare} id="shareform">
+                    <h6>Data Hash</h6>
+                    <input type='text' onChange={this.captureFile} />
+                    <h6>Recipient Public Key</h6>
+                    <input type='text' onChange={this.captureFile} />
+                    <h6>Data Hash</h6>
+                    <input type='text' onChange={this.captureFile} />
                     <input type='submit' />
                 </form>
             </div>
